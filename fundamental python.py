a3b1b4b8d2193e5b07d018b5105f06e020de673e
aprinur <aprinur7@gmai.com>
2023-03-30 11:29:07 +0700

    add title

---
+++ fundamental python.py
@@ -0,0 +1,14 @@
+"""
+Semua sintaksis dasar bahasa pemrograman terdiri dari :
+1. Sekuensial : langkah berurutan
+2. Percabangan : langkah melompat jika kondisi terpenuhi
+3. Perulangan : mengulang langkah yang sama berkali kali selama/sampai kondisi terpenuhi
+"""
+#Sekuensial
+print('ibu berkata,"Pergi ke toko"')
+print('Budi menjawab,"Apa yang harus saya lakukan di toko?"')
+print('Ibu mejawab, "Beli satu botol susu, jika ada telur belu 6"')
+print("Maka budi berangkat ke toko")
+print("Budi mengecek harga susu ternyata cukup")
+print("Budi tidak menemukan telur lalu membeli sebotol susu")
+print("Budi pulang menyerahkan belanjaan kepada ibu")
