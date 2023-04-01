d06360ad3eb4471271fc6f743fd964e0ef94d2b4
aprinur <aprinur7@gmai.com>
2023-04-01 15:52:01 +0700

    melengkapi

--- fundamental python.py
+++ fundamental python.py
@@ -4,11 +4,29 @@ Semua sintaksis dasar bahasa pemrograman terdiri dari :
 2. Percabangan : langkah melompat jika kondisi terpenuhi
 3. Perulangan : mengulang langkah yang sama berkali kali selama/sampai kondisi terpenuhi
 """
-#Sekuensial#
+# Sekuensial
 print('ibu berkata,"Pergi ke toko"')
 print('Budi menjawab,"Apa yang harus saya lakukan di toko?"')
-print('Ibu mejawab, "Beli satu botol susu, jika ada telur belu 6"')
+print('Ibu mejawab, "Beli satu botol susu, jika ada telur bel1 6 "')
 print("Maka budi berangkat ke toko")
-print("Budi mengecek harga susu ternyata cukup")
-print("Budi tidak menemukan telur lalu membeli sebotol susu")
-print("Budi pulang menyerahkan belanjaan kepada ibu")
+
+
+# Percabangan
+jumlah_botol_susu = y = 12
+jumlah_telur = x = 5
+print(fr"Jumlah botol susu {y} botol")
+print(f'jumlah telur {jumlah_telur} butir')
+if jumlah_botol_susu > 0:
+    print('budi mengecek harga 1 botol susu')
+    print('budi membeli 1 botol susu')
+else:
+    print('budi tidak jadi membeli susu')
+if jumlah_telur > 6:
+    print('budi membeli 6 buah telur')
+if jumlah_telur > 0 < 6:
+    print(f"budi membeli {x} butir telur")
+else:
+    print('budi hanya membeli 1 botol susu')
+
+
+print('budi pulang dan menyerahkan hasil belanjaan kepada ibu')
