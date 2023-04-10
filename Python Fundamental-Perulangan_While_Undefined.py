e229a5114733caa1850d730a844458008b2b77ac
aprinur <aprinur7@gmai.com>
2023-04-10 12:31:04 +0700

    Penyingkatan Variabel

--- Python Fundamental-Perulangan_While_Undefined.py
+++ Python Fundamental-Perulangan_While_Undefined.py
@@ -4,21 +4,21 @@ Contoh perulangan dengan While
 
 Jumlah_Buku = 10
 print('Ibu berkata, "Baca semua bukumu sampai paham"')
-total_jumlah_baca = 0
+Jumlah_baca = 0
 
-Jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
-print(f'Jumlah buku yang sudah dibaca dan dipahami {Jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
+Jumlah_paham = 0
+print(f'Jumlah buku yang sudah dibaca dan dipahami {Jumlah_paham}')
 
-while total_jumlah_baca < Jumlah_Buku * 2:
-    total_jumlah_baca = total_jumlah_baca + 1
-    if Jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9:
-        print(f"Buku ke { Jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1} belum paham")
+while Jumlah_baca < Jumlah_Buku * 2:
+    Jumlah_baca = Jumlah_baca + 1
+    if Jumlah_paham == 9:
+        print(f"Buku ke {Jumlah_paham + 1} belum paham")
     else:
-        Jumlah_buku_yang_sudah_dibaca_dan_dipahami = Jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
-        print(f"Buku ke {Jumlah_buku_yang_sudah_dibaca_dan_dipahami } sudah dibaca dan dipahami")
+        Jumlah_paham = Jumlah_paham + 1
+        print(f"Buku ke {Jumlah_paham } sudah dibaca dan dipahami")
 
-print(f'Jumlah buku yang sudah dibaca dan dipahami {Jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
-if total_jumlah_baca == Jumlah_Buku:
+print(f'Jumlah buku yang sudah dibaca dan dipahami {Jumlah_paham}')
+if Jumlah_baca == Jumlah_Buku:
     print("Bu, semua buku sudah dibaca dan dipahami")
 else:
     print("Bu, tidak semua buku dipahami")
