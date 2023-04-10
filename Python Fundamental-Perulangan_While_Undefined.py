24980825340d5fc6e1bccbcc5b00ae4fee3b8882
aprinur <aprinur7@gmai.com>
2023-04-10 12:34:39 +0700

    Variabel ganti basa inggris

--- Python Fundamental-Perulangan_While_Undefined.py
+++ Python Fundamental-Perulangan_While_Undefined.py
@@ -2,23 +2,23 @@
 Contoh perulangan dengan While
 """
 
-Jumlah_Buku = 10
+Book_amount = 10
 print('Ibu berkata, "Baca semua bukumu sampai paham"')
-Jumlah_baca = 0
+Readed_book = 0
 
-Jumlah_paham = 0
-print(f'Jumlah buku yang sudah dibaca dan dipahami {Jumlah_paham}')
+Understood = 0
+print(f'Jumlah buku yang sudah dibaca dan dipahami {Understood}')
 
-while Jumlah_baca < Jumlah_Buku * 2:
-    Jumlah_baca = Jumlah_baca + 1
-    if Jumlah_paham == 9:
-        print(f"Buku ke {Jumlah_paham + 1} belum paham")
+while Readed_book < Book_amount * 2:
+    Readed_book = Readed_book + 1
+    if Understood == 9:
+        print(f"Buku ke {Understood + 1} belum paham")
     else:
-        Jumlah_paham = Jumlah_paham + 1
-        print(f"Buku ke {Jumlah_paham } sudah dibaca dan dipahami")
+        Understood = Understood + 1
+        print(f"Buku ke {Understood } sudah dibaca dan dipahami")
 
-print(f'Jumlah buku yang sudah dibaca dan dipahami {Jumlah_paham}')
-if Jumlah_baca == Jumlah_Buku:
+print(f'Jumlah buku yang sudah dibaca dan dipahami {Understood}')
+if Readed_book == Book_amount:
     print("Bu, semua buku sudah dibaca dan dipahami")
 else:
     print("Bu, tidak semua buku dipahami")
