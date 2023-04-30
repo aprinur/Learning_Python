9f28596c530345999828ff254afa582b3dd423e5
aprinur <aprinur7@gmai.com>
2023-04-30 19:54:20 +0700

    Dictionary

---
+++ dasar_sintaksis_dictionary.py
@@ -0,0 +1,15 @@
+# Tipe data dictionary
+
+users = {   # Penulisan dictionary menggunakan kurung kurawal
+    'id': 1,
+    'name': 'Leanne Graham',
+    'username': 'Bret',
+    'email': 'Sincere@april.biz',
+}
+print(users)  # Contoh kode untuk menampilkan seluruh isi dictionary
+print(users['id'])
+print(users['name'])
+print(users['username'])   # Contoh kode untuk menampilkan salah 1 isi dictionary,id bisa diganti key yang lain
+print(users['email'])
+
+# Key = id, name, username, email dll. Key harus text tidak boleh angka
