6e027d420db3d1ef686ccb3c1508873689f16c38
aprinur <aprinur7@gmai.com>
2023-05-01 18:51:30 +0700

    Penambahan Json yang lebih kompleks

--- Python Fundamental-Perulangan_While.py
+++ Python Fundamental-Perulangan_While.py
@@ -2,13 +2,13 @@
 Contoh perulangan dengan While
 """
 
-Jumlah_Buku = 100
+Jumlah_buku = 100
 print('Ibu berkata, "Baca semua bukumu"')
 
 Jumlah_buku_yang_sudah_dibaca = 0
 print(f'Jumlah buku yang sudah dibaca {Jumlah_buku_yang_sudah_dibaca}')
 
-while Jumlah_buku_yang_sudah_dibaca < Jumlah_Buku:
+while Jumlah_buku_yang_sudah_dibaca < Jumlah_buku:
     Jumlah_buku_yang_sudah_dibaca = Jumlah_buku_yang_sudah_dibaca + 1
     print(f"Buku ke {Jumlah_buku_yang_sudah_dibaca} sudah dibaca")
 
--- dasar_sintaksis_dictionary.py
+++ dasar_sintaksis_dictionary.py
@@ -1,15 +1,28 @@
 # Tipe data dictionary
 
 users = {   # Penulisan dictionary menggunakan kurung kurawal
-    'id': 1,
-    'name': 'Leanne Graham',
-    'username': 'Bret',
-    'email': 'Sincere@april.biz',
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "City": "Gwenborough",
+        "Zipcode": "92998-3874",
+        "Geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+    }
 }
 print(users)  # Contoh kode untuk menampilkan seluruh isi dictionary
-print(users['id'])
-print(users['name'])
-print(users['username'])   # Contoh kode untuk menampilkan salah 1 isi dictionary,id bisa diganti key yang lain
-print(users['email'])
+print(users["id"])
+print(users["name"])
+print(users["username"])   # Contoh kode untuk menampilkan salah 1 isi dictionary,id bisa diganti key yang lain
+print(users["email"])
+print(users["address"]["City"])
+print(users["address"]["Geo"]["lng"])  # Contoh kode untuk menampilkan isi dictionary yang lebih spesifik
 
 # Key = id, name, username, email dll. Key harus text tidak boleh angka
+# Dictionary = Python, Json = semua bahasa pemrograman
